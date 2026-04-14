a046011cb95c7848f819f0500263447d01ac02e1
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-04-14 09:56:11 +0300

    work commit

--- Lab8CSharp/Program.cs
+++ Lab8CSharp/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#8 ");
-//  За бажанням студента косольний проект або WinForm
+﻿using HexIpProcessor;
+
+namespace ConsoleApp2;
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Lab 4 - меню");
+            Console.WriteLine("3 - Task 3 (MatrixShort)");
+            Console.WriteLine("2 - Task 2 (VectorShort)");
+            Console.WriteLine("1 - Task 1 (Rectangle - базові операції + нові операції)");
+            Console.WriteLine("0 - Вихід");
+            Console.Write("Ваш вибір: ");
+
+            string? choice = Console.ReadLine();
+
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case "3":
+                    break;
+
+                case "2":
+                    break;
+
+                case "1":
+                    task_1.Run(args);
+                    break;
+                
+
+                case "0":
+                    return;
+
+                default:
+                    Console.WriteLine("Невірний вибір. Спробуйте ще раз.");
+                    break;
+            }
+        }
+    }
+}
